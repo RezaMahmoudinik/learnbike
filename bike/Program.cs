0feb9caa2d05087dbe2880a92b4e5ddafd670713
reza <rezamahmoudinik1380@gmail.com>
2021-12-25 16:40:48 +0330

    commiting from pc

---
+++ bike/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the number of items:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            class_anbar[] items = new class_anbar[number];
+             
+
+                for(int i = 0; i < number; i++)
+            {
+                Console.WriteLine($"please enter name of items {(i+1)}:");
+                string name = Console.ReadLine();
+
+                Console.WriteLine($"please enter model of items {(i + 1)}:");
+                string model = Console.ReadLine();
+
+                class_anbar anbar = new class_anbar(name, model);
+                items[i]= anbar;
+
+            }
+            
+               foreach(class_anbar class_Anbar in items)
+            {
+                Console.WriteLine($"name :{class_Anbar.name} model:{class_Anbar.model}");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ bike/class_anbar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bike
+{
+    class class_anbar
+    {
+        public string name;
+        public string model;
+      
+
+
+        public class_anbar(string name,string model)
+        {
+            this.name = name;
+            this.model = model;
+           
+
+        }
+
+    }
+}
